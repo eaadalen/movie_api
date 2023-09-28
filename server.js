fc8cb22c975e63bac656eee53aff336a9d8124ce
eaadalen <eaadalen112@gmail.com>
2023-09-28 13:13:31 -0500

    2.2 revisions V4

--- server.js
+++ server.js
@@ -1,37 +1,27 @@
 const http = require("http");
+const url = require("url");
 const fs = require("fs");
 
 http.createServer((request, response) => {
-    console.log(request.url);
 
-    /*let url = new URL(request.url, `http://${request.headers.host}`);
-    console.log(url);
-    let filePath = "";
+  let url = new URL(request.url, `http://${request.headers.host}`);
+  console.log(url);
+  let filePath = "";
 
-    if (url.pathname.includes("documentation")) {
-      filePath = __dirname + "/documentation.html";
-      console.log(filePath);
-    } 
+  if (url.pathname.includes("documentation")) {
+    filePath = __dirname + "/documentation.html";
+  } 
 
-    fs.appendFile(
-      "log.txt",
-      `URL: ${request.url} \nTimestamp: ${new Date()} \n \n`,
-      (err) => {
-        if (err) {
-          console.log("append error");
-        } 
-        else {
-          console.log("msg appended");
-        }
-      }
-    );
-
-    fs.readFile(filePath, (err, data) => {
+  fs.appendFile("log.txt",`URL: ${request.url} \nTimestamp: ${new Date()} \n \n`, (err) => {
       if (err) {
-        throw err;
+        console.log("append error");
+      } 
+      else {
+        console.log("msg appended");
       }
-      response.writeHead(200, { "Content-Type": "text/html" });
-      response.write(data);
-      response.end("hello node");
-    });*/
-  }).listen(8080);
+    }
+  );
+
+}).listen(8080);
+
+console.log("Listening on port 8080...");
