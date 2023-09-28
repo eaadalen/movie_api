ed77da723f63dca9c33be1bc4846876729b5111a
eaadalen <eaadalen112@gmail.com>
2023-09-28 11:57:29 -0500

    2.2 revisions

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const fs = require("fs");
 
 http.createServer((request, response) => {
     let url = new URL(request.url, `http://${request.headers.host}`);
+    console.log(url);
     let filePath = "";
 
     if (url.pathname.includes("documentation")) {
       filePath = __dirname + "/documentation.html";
+      console.log(filePath);
     } 
-    else {
-      filePath = "index.html";
-    }
 
+    /*
     fs.appendFile(
       "log.txt",
       `URL: ${request.url} \nTimestamp: ${new Date()} \n \n`,
@@ -32,5 +32,5 @@ http.createServer((request, response) => {
       response.writeHead(200, { "Content-Type": "text/html" });
       response.write(data);
       response.end("hello node");
-    });
-  }).listen(8080);
+    });*/
+  }).listen(3000);
