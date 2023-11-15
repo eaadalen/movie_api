5b53672806fa396370cd75daf61151f45df58442
eaadalen <eaadalen112@gmail.com>
2023-11-14 21:26:13 -0600

    Add netlify to CORS

--- index.js
+++ index.js
@@ -9,8 +9,18 @@ const Movies = Models.Movie;
 const Users = Models.User;
 const app = express();
 const cors = require('cors');
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://myflix-client-eaadalen.netlify.app'];
 const port = process.env.PORT || 8080;
-app.use(cors());
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'));
